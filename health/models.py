a658cdf1bfc456997676b7b2ca5f0bc0ecf814df
manju <manjunathdc1997@gmail.com>
2019-02-11 17:27:14 +0530

    healthcare_insurance full project

--- health/models.py
+++ health/models.py
@@ -1,3 +1,10 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+	"""username = models.charField(max_length=200)
+	password = models.charField(max_length=200)"""
+	login = models.TextField()
+	def __init__(self):
+		return self.login
+
--- health/views.py
+++ health/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
+from .models import Post
+from django.shortcuts import redirect
 
-def index(request):
-	return render(request, 'health/index.html', {})
+def base(request):
+    return render(request, 'health/base.html', {})
+def index1(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            return redirect('index1')
+        else:
+            form = PostForm()
+    return render(request, 'health/index1.html', {})
+def login(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+    return render(request, 'health/login.html', {})
